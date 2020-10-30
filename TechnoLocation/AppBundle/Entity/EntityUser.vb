0b8e7fd7c57b50548f26ba2121871a7585536deb
Glackssel <glackssel@gmail.com>
2020-10-30 11:13:42 -0400

    Section User
    Recherche d'utilisateur
    filtre de recherche
    Suppression d'utilisateur
    Front end ajout utilisateur

--- TechnoLocation/AppBundle/Entity/EntityUser.vb
+++ TechnoLocation/AppBundle/Entity/EntityUser.vb
@@ -32,7 +32,7 @@ Public Class EntityUser
         End If
         Dim command As New MySqlCommand
         command.Connection = connection
-        command.CommandText = $"Select * from user U order by U.code"
+        command.CommandText = $"Select code, firstName, lastName, email, phoneMain, phone2, job, permissions, balance from user U order by U.code"
         connection.Open()
         Dim reader = command.ExecuteReader()
         Dim table As New DataTable("users")
@@ -48,6 +48,96 @@ Public Class EntityUser
         Dim command As New MySqlCommand
         command.Connection = connection
         command.CommandText = $"Select code, password from user"
+        connection.Open()
+        Dim reader = command.ExecuteReader()
+        Dim table As New DataTable("users")
+        table.Load(reader)
+        connection.Close()
+        Return table
+    End Function
+
+    Public Function getUsersCode(code As Integer) As DataTable
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+        End If
+        Dim command As New MySqlCommand
+        command.Connection = connection
+        command.CommandText = $"Select code, firstName, lastName, email, phoneMain, phone2, job, permissions, balance from user U where code = '{code}' order by U.code"
+        connection.Open()
+        Dim reader = command.ExecuteReader()
+        Dim table As New DataTable("users")
+        table.Load(reader)
+        connection.Close()
+        Return table
+    End Function
+
+    Public Function getUsersFirstName(firstName As String) As DataTable
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+        End If
+        Dim command As New MySqlCommand
+        command.Connection = connection
+        command.CommandText = $"Select code, firstName, lastName, email, phoneMain, phone2, job, permissions, balance from user U where firstName like '{firstName}%' order by U.code"
+        connection.Open()
+        Dim reader = command.ExecuteReader()
+        Dim table As New DataTable("users")
+        table.Load(reader)
+        connection.Close()
+        Return table
+    End Function
+
+    Public Function getUsersLastName(lastName As String) As DataTable
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+        End If
+        Dim command As New MySqlCommand
+        command.Connection = connection
+        command.CommandText = $"Select code, firstName, lastName, email, phoneMain, phone2, job, permissions, balance from user U where lastName like '{lastName}%' order by U.code"
+        connection.Open()
+        Dim reader = command.ExecuteReader()
+        Dim table As New DataTable("users")
+        table.Load(reader)
+        connection.Close()
+        Return table
+    End Function
+
+    Public Function getUsersEmail(email As String) As DataTable
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+        End If
+        Dim command As New MySqlCommand
+        command.Connection = connection
+        command.CommandText = $"Select code, firstName, lastName, email, phoneMain, phone2, job, permissions, balance from user U where email like '{email}%' order by U.code"
+        connection.Open()
+        Dim reader = command.ExecuteReader()
+        Dim table As New DataTable("users")
+        table.Load(reader)
+        connection.Close()
+        Return table
+    End Function
+
+    Public Function getUsersPhone(phone As String) As DataTable
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+        End If
+        Dim command As New MySqlCommand
+        command.Connection = connection
+        command.CommandText = $"Select code, firstName, lastName, email, phoneMain, phone2, job, permissions, balance from user U where phoneMain like '{phone}%' || phone2 like '{phone}%' order by U.code"
+        connection.Open()
+        Dim reader = command.ExecuteReader()
+        Dim table As New DataTable("users")
+        table.Load(reader)
+        connection.Close()
+        Return table
+    End Function
+
+    Public Function getUsersJob(job As String) As DataTable
+        If connection.State = ConnectionState.Open Then
+            connection.Close()
+        End If
+        Dim command As New MySqlCommand
+        command.Connection = connection
+        command.CommandText = $"Select code, firstName, lastName, email, phoneMain, phone2, job, permissions, balance from user U where job like '{job}' order by U.code"
         connection.Open()
         Dim reader = command.ExecuteReader()
         Dim table As New DataTable("users")
